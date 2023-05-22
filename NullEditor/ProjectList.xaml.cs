ed56d6972d487b6cf6981d55883a2e8018390363
sametydev <108689707+sametydev@users.noreply.github.com>
2023-05-22 03:14:37 +0300

    Some refactoring bcs im bored.

--- NullEditor/ProjectList.xaml.cs
+++ NullEditor/ProjectList.xaml.cs
@@ -14,6 +14,8 @@ using System.Windows.Shapes;
 using System.Text.RegularExpressions;
 using NullEditor.Project;
 using System.IO;
+using NullEditor.Engine;
+using NullEditor.Other;
 
 namespace NullEditor
 {
@@ -26,50 +28,11 @@ namespace NullEditor
             UpdateProjectListView();
 
             ProjectManager.Instance.OnProjectCreated += UpdateProjectListView;
+            ProjectManager.Instance.OnProjectListLoaded += UpdateProjectListView;
         }
 
         private void CreateProjectButtonOnClick(object sender, RoutedEventArgs e)
-        {
-            string projectName = projectNameTextBox.Text;
-
-            if (!Regex.IsMatch(projectName, Global.PROJECT_NAME_REGEX))
-            {
-                MessageBox.Show("Project Name Contains Spaces or Special Characters!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
-                return;
-            }
-
-            Project.Project project = new Project.Project();
-            project.Name = projectName;
-            project.Description = "A NullEngine Project";
-            project.Version = Global.ENGINE_VERSION;
-
-            //If Projects Directory is not exists!
-            if (!Directory.Exists("Projects"))
-            {
-                Directory.CreateDirectory("Projects");
-            }
-            //If project exists from disk not memory
-            if (File.Exists(string.Format("Projects/{0}.json",projectName)))
-            {
-                MessageBox.Show("Project Exists on Disk!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
-                return;
-            }
-
-            //If everything is ok;
-            string projectFileJson = string.Format("Projects/{0}/{0}.json", projectName);
-
-            Directory.CreateDirectory(string.Format("Projects/{0}", projectName));
-            File.Create(projectFileJson);
-            FileInfo finfo = new FileInfo(projectFileJson);
-            project.Path = finfo.FullName;
-            ProjectManager.Instance.CreateProject(project);
-
-            //Copy Default project
-
-            //Go To Editor;
-
-            MessageBox.Show("Project is created!");
-        }
+        => ProjectManager.Instance.CreateProjectOnDisk(projectNameTextBox.Text);
 
         public void UpdateProjectListView()
         {
