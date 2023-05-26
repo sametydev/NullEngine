3c92bb02ff351ace26e7d39c678d5f5ba64901fd
sametydev <108689707+sametydev@users.noreply.github.com>
2023-05-26 16:54:03 +0300

    Some Fonts added and Font Class Update

--- NullEngine/Src/Render/Font.cpp
+++ NullEngine/Src/Render/Font.cpp
@@ -6,7 +6,7 @@ Font::Font()
 {
 }
 
-void Font::Create(LPCSTR filename)
+void Font::Create(LPCSTR filename,uint size)
 {
 	FT_Library lib;
 	FT_Face face;
@@ -17,5 +17,53 @@ void Font::Create(LPCSTR filename)
 	if (FT_New_Face(lib,filename,0,&face))
 	{
 		//assert
+	}
+
+	FT_Set_Pixel_Sizes(face,0,size);
+
+	if (FT_Load_Char(face,'X',FT_LOAD_RENDER))
+	{
+		//assert
+	}
+	FT_GlyphSlot glyph = face->glyph;
+
+	uint width = 0;
+	uint height = 0;
+
+	//calc total with height
+	for (int c = FONT_MIN_CHAR; c < FONT_MAX_CHAR; c++)
+	{
+		if (FT_Load_Char(face,c,FT_LOAD_RENDER))
+		{
+			continue;
+		}
+		width += glyph->bitmap.width;
+		height = std::max(height,glyph->bitmap.rows);
+
+	}
+
+	byte* buffer = new byte[width*height];
+
+	memset(buffer, byte(0), sizeof(byte) * width * height);
+
+	int offsetx = 0;
+	int offsety = 0;
+
+	for (int c = FONT_MIN_CHAR; c < FONT_MAX_CHAR; c++)
+	{
+		if (FT_Load_Char(face,c,FT_LOAD_RENDER))
+		{
+			continue;
+		}
+
+		FT_Bitmap& bitmap = glyph->bitmap;
+
+		for (int y = 0; y < bitmap.rows; y++)
+		{
+			for (int x = 0; x < bitmap.width; x++)
+			{
+				buffer[(y * width) + (offsetx + x)] = bitmap.buffer[y * bitmap.width + x];
+			}
+		}
 	}
 }
--- NullEngine/Src/Render/Font.h
+++ NullEngine/Src/Render/Font.h
@@ -1,12 +1,14 @@
 #pragma once
 
 #pragma comment(lib,"freetype.lib")
+constexpr int FONT_MIN_CHAR = 32;
+constexpr int FONT_MAX_CHAR = 128;
 
 class Font
 {
 public:
 	Font();
 
-	void Create(LPCSTR filename);
+	void Create(LPCSTR filename,uint size);
 };
 
